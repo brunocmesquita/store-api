17ab18c2a25836cba6c592f4a983756dc634bf61
Bruno Mesquita <brunocmesquita@gmail.com>
2021-07-18 10:57:32 -0300

    insertClient

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,9 +1,19 @@
-import pg from 'pg';
-
-
+import { connect } from './db.js';
 
 async function insertClient(client) {
+  const conn = await connect();
+  const sql =
+    'INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+  const values = [
+    client.name,
+    client.cpf,
+    client.phone,
+    client.email,
+    client.address,
+  ];
 
+  const res = await conn.query(sql, values);
+  return res.rows;
 }
 
 export default {
