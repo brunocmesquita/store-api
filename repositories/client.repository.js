35cf8f6e2d07364614f2fb1fbced8f0c2b1c793d
Bruno Mesquita <brunocmesquita@gmail.com>
2021-07-18 11:03:08 -0300

    insertClient updated

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -2,18 +2,24 @@ import { connect } from './db.js';
 
 async function insertClient(client) {
   const conn = await connect();
-  const sql =
-    'INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-  const values = [
-    client.name,
-    client.cpf,
-    client.phone,
-    client.email,
-    client.address,
-  ];
+  try {
+    const sql =
+      'INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *';
+    const values = [
+      client.name,
+      client.cpf,
+      client.phone,
+      client.email,
+      client.address,
+    ];
 
-  const res = await conn.query(sql, values);
-  return res.rows;
+    const res = await conn.query(sql, values);
+    return res.rows;
+  } catch (err) {
+    throw err;
+  } finally {
+    conn.release();
+  }
 }
 
 export default {
